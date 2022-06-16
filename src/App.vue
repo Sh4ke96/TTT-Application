589bdb075276ad2fa778e7b761c8990ad97e1c43
Artur Michałek <sh4ke96@gmail.com>
2022-06-16 13:18:30 +0200

    add fully working game with reset button and styles

--- src/App.vue
+++ src/App.vue
@@ -26,27 +26,66 @@ const calculateWinner = (squares) => {
     }
   }
   return null;
+};
 
-  const winner = computed(() => calculateWinner(board.value.flat()));
-  const moves = (x, y) => {
-    if (winner.value) return;
-    if (board.value[x][y] !== "") return;
-    board.value[x][y] = player.value;
-    player.value = player.value === "X" ? "O" : "X";
-  };
-  const reset = () => {
-    board.value = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-    ];
-    player.value = "X";
-  };
+const winner = computed(() => calculateWinner(board.value.flat()));
+
+const moves = (x, y) => {
+  if (winner.value) return;
+  if (board.value[x][y]) return;
+  board.value[x][y] = player.value;
+  player.value = player.value === "X" ? "O" : "X";
+};
+
+const reset = () => {
+  board.value = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  player.value = "X";
 };
 </script>
 
 <template>
-  <h1>Hello Vue World</h1>
+  <main class="pt-8 text-center dark:bg-gray-900 min-h-screen dark:text-white">
+    <h1 class="mb-12 text-5xl font-bold uppercase underline decoration-orange-500">
+      Tic Tac Toe
+    </h1>
+    <h3 class="text-3xl mb-4">Player {{ player }}'s turn</h3>
+    <div class="flex flex-col items-center mb-8">
+      <div v-for="(row, x) in board" :key="x" class="flex">
+        <div
+          v-for="(cell, y) in row"
+          :key="y"
+          @click="moves(x, y)"
+          :class="`border border-white w-20 h-20 hover:bg-gray-700 flex items-center justify-center material-icons-outlined text-4xl cursor-pointer ${
+            cell === 'X' ? 'text-orange-500' : 'text-yellow-500'
+          }`"
+        >
+          {{ cell === "X" ? "close" : cell === "O" ? "circle" : "" }}
+        </div>
+      </div>
+    </div>
+    <h2 v-if="winner" class="text-6xl font-bold mb-8">
+      Player {{ winner }} wins!
+    </h2>
+    <button
+      @click="reset()"
+      class="
+        px-8
+        py-6
+        bg-orange-600
+        font-bold
+        text-2xl
+        uppercase
+        hover:bg-orange-400
+        duration-300
+      "
+    >
+      Reset Game
+    </button>
+  </main>
 </template>
 
 <style>
